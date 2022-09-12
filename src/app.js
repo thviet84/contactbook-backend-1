f6906016eadde86afad0f9af5ca2e38cd43061a2
Tran Hoang Viet <viethoangtv84@gmail.com>
2022-09-12 09:59:04 +0700

    Implement an error handling middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const cors = require('cors');
 const contactController = require('./controllers/contact.controller');
+const ApiError = require('./api-error');
 
 const app = express();
 
@@ -23,5 +24,27 @@ app. route('/api/contacts/:id')
     .put(contactController.update) 
     .delete(contactController. delete);
 
+app. route('/api/contacts/:id')
+    .get(contactController.findone) 
+    .put(contactController.update) 
+    .delete(contactController.delete);
+
+// Handle 404 response. 
+app.use((req, res, next) => {
+// Handler for unknown route. 
+    // call next() to pass to the error handling middleware.
+    return next(new ApiError(404, 'Resource not found')); 
+});
+
+// Define error-handling middleware last, after other app.use() and routes calls. 
+app.use((err, req, res, next) => {
+// The centralized error handling middleware. 
+// In any route handler, calling next(error)
+// will pass to this error handling middleware. 
+    return res.status (error.statusCode || 500).json({
+        message: error.message || 'Internal Server Error', 
+    });
+});
+    
 module.exports = app;
 
